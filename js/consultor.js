923eeaa9a9c8da12c27772fa5f743f703202764b
Willians <williansferr@gmail.com>
2018-08-19 11:52:01 -0300

    Efeitos de span e imagem adicionados

--- js/consultor.js
+++ js/consultor.js
@@ -1,8 +1,47 @@
 var slideIndex = 1;
 showDivs(slideIndex);
-
+var path_imgTh = "./img/thiago2-1.jpg";
 var myIndex = 0;
 carousel();
+
+
+
+document.getElementById('home').addEventListener("mouseenter", function(){
+  view_span_left();
+});
+
+document.getElementById('myNavbar').addEventListener("mouseenter", function(){
+  view_span_top();
+  $( "#imagem-about" ).attr( "src", path_imgTh);
+
+});
+
+document.getElementById('myNavbar').addEventListener("mouseleave", function(){
+  $( "#imagem-about" ).attr( "src", path_imgTh );
+
+});
+
+var loadElement_sobre = $('#sobre');
+var loadElement_about = $('#about');
+
+loadElement_sobre.on('click',function(){
+  load_img_about_top();
+})
+
+$( "#imagem-about" ).attr( "src", "");
+
+loadElement_about.on('mouseenter',function(){
+  setTimeout(function(){
+    load_img_about_left();
+  },1000);
+})
+
+loadElement_about.on('mouseleave',function(){
+  $( "#imagem-about" ).attr( "src", "");
+})
+
+
+
 
 function carousel() {
     var i;
@@ -13,7 +52,7 @@ function carousel() {
     myIndex++;
     if (myIndex > x.length) {myIndex = 1}    
     x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 2000); // Change image every 2 seconds
+    setTimeout(carousel, 3000); // Change image every 2 seconds
 }
 
 function plusDivs(n) {
@@ -94,3 +133,47 @@ function myFunction(id) {
         x.previousElementSibling.className.replace("w3-red", "w3-black");
     }
 }
+
+
+
+function load_img_about_left(){
+  $( "#imagem-about" ).attr( "src", path_imgTh);
+  var img_thiago = document.getElementById('imagem-about');
+  img_thiago.classList.add('w3-animate-left');
+  setTimeout(function(){
+  img_thiago.classList.remove('w3-animate-left');
+  },200);
+
+}
+
+function load_img_about_top(){
+  $( "#imagem-about" ).attr( "src", path_imgTh );
+  var img_thiago = document.getElementById('imagem-about');
+  img_thiago.classList.add('w3-animate-top');
+  setTimeout(function(){
+  img_thiago.classList.remove('w3-animate-top');
+  },200);
+
+}
+
+function view_span_top(){
+  var span = $('span');
+  span.addClass('w3-animate-top');
+  setTimeout(function(){
+  span.removeClass('w3-animate-top');
+  },300);
+
+}
+
+function view_span_left(){
+  var span = $('span');
+  span.addClass('w3-animate-left');
+  setTimeout(function(){
+  span.removeClass('w3-animate-left');
+  },300);
+
+}
+
+
+
+
